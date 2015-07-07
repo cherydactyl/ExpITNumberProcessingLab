9ef48e240bcacdc2eda7b04707cd55998984ed18
cherydactyl <cheryl.orosz@gmail.com>
2015-07-07 16:05:40 -0400

    Space Exploration
    Added part 3, Space Exploration

--- ExpITNumberProcessingLab/Program.cs
+++ ExpITNumberProcessingLab/Program.cs
@@ -12,7 +12,6 @@ namespace ExpITNumberProcessingLab
         {
             /* Accumulation
              * 
-             * Problem definition:
              * Every day, a woman deposits $35.00 in her bank account.  Write a program that
              * prints out how much money she has after 1 year, 2 years, 5 years, and 10 years.
              * Assume she starts her deposits on January 1, 2015
@@ -22,14 +21,14 @@ namespace ExpITNumberProcessingLab
             // My answer: per the reccomendation we were given, fixed point is a good type for currency,
             // however, I have used a float for this solution.
 
-            Console.WriteLine("Accumulation");
+            Console.WriteLine("**PART 1: Accumulation");
             Console.WriteLine("A woman deposits $35.00 in her bank account every day.");
             Console.WriteLine("(Note: this solution assumes no interest is earned on the deposits.)");
             Console.WriteLine();    //whitespace
             //define datetime for January 1, 2015
             DateTime startDate = new DateTime(2015, 1, 1);
-            float perDiem = (float) 35.00;
-            
+            float perDiem = (float)35.00;
+
             Console.WriteLine("If she starts on " + startDate.ToShortDateString() + " then,");
 
             //after 1 year
@@ -56,7 +55,7 @@ namespace ExpITNumberProcessingLab
              * In a far away land, people pay for land by a combination of the parcel's area and its perimeter.
              * Land costs $5 per square yard plus $0.75 per foot of the perimeter.
              */
-            Console.WriteLine("Land Cost Calculator");
+            Console.WriteLine("**Part 2: Land Cost Calculator");
             Console.WriteLine();    //Whitespace
 
             Console.WriteLine("To calculate the cost of a parcel, please input its dimensions in whole feet (numbers only please):");
@@ -64,7 +63,7 @@ namespace ExpITNumberProcessingLab
             int length = int.Parse(Console.ReadLine());
             Console.WriteLine("What is the width in feet?");
             int width = int.Parse(Console.ReadLine());
-            
+
             //calculate area in square feet
             int area = length * width;
 
@@ -77,37 +76,80 @@ namespace ExpITNumberProcessingLab
 
             Console.WriteLine("The cost for a parcel " + width + " feet  by " + length + " feet is $" + cost + ".");
 
+            Console.WriteLine();    //whitespace
+            Console.WriteLine();    //whitespace
 
+            Console.WriteLine("**Part 3: Space Exploration");
+            Console.WriteLine();    //Whitespace
+            /*Space Exploration
+             * Once in space, a space ship can increase its velocity by 5% by firing its boosters for 30 seconds.
+             * Assume the space craft is travelleing at 10,000 miles per hour.
+             * 
+             * How fast will it be traveling after 2 hours
+            */
+
+            //because we are starting with a fairly high, round number, an integer will do for this purpose
+            int velocity = 10000;
+
+            Console.WriteLine("For a ship starting at velocity 10,000 mph,\n" +
+                "if it can increase its velocity by 5% for each 30 seconds,");
+
+            //loop over t, where t is time in seconds
+            //remember that 5 minutes is 300 seconds
+            for (int t = 0; t < 300; t += 30)
+            {
+                velocity += velocity / 20;    //adding 5% is the same as adding 1/20th
+                //Console.WriteLine(velocity);  //used for debugging checks
+            }
+            Console.WriteLine("Then after 5 minutes of firing its boosters, \nthe craft will be traveling at " +
+                velocity + " miles per hour");
+
+            Console.WriteLine();    //whitespace
+            Console.WriteLine("For a ship starting at velocity 10,000 mph,\n" +
+                "if it can increase its velocity by 5% for each 30 seconds,\n" +
+                "but cannot fire continuously, imagine that the craft fires boosters\n" +
+                "for 5 minutes, then coasts for 10 minutes, and repeats the cycle \n" +
+                "for a total of two hours.");
+
+            //reset the velocity for the new situation
+            velocity = 10000;
+
+            //loop over t, where t is time in seconds
+            //remember that 5 minutes is 300 seconds, and 10 minutes is 600 seconds
+            int endTime = 2 * 60 * 60;  //calculate two hours in seconds, for convience and readability
+            //Console.WriteLine("Two hours in seconds is " + endTime);    //for debugging help
+
+            //each cycle is 15 minutes: 5 of burn and 10 of coast
+            //outer loop will increment t for coasting
+            for (int t = 0; t < endTime; t += 600)
+            {
+                //Console.WriteLine("At time " + t + " seconds, the velocity is " + velocity);
+
+                //burn for 5 minutes
+                //inner loop will increment t for burn time
+                for (int endBurn = t + 300; t < endBurn; t += 30)
+                {
+                    velocity += velocity / 20;    //adding 5% is the same as adding 1/20th
+                    //Console.WriteLine(velocity);  //used for debugging checks
+                }
+                //Console.WriteLine("At time " + t + " seconds, the velocity is " + velocity);
+            }
+
+            Console.WriteLine();    //Whitespace
+            Console.WriteLine("At then end of the two hours, the craft is traveling at "
+                + velocity + " miles per hour.");
+
+
+            //Hold the console open until the user is ready to exit.
             Console.WriteLine();    //whitespace
             Console.WriteLine();    //whitespace
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
-
         }
 
-        static double calculateBalance(float increment, DateTime startDate, DateTime endDate )
+        static double calculateBalance(float increment, DateTime startDate, DateTime endDate)
         {
             return ((endDate - startDate).TotalDays * increment);
         }
-
-        
-
-
-
-        
-    
-        
-
-        //Land Cost Calculator
-
-
-        //Space Exploration
-
-    
-    
-}
-
-    
-
-
+    }
 }
